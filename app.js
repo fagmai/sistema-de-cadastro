99212c8a0571500f09bff36cbdeb8dad3f6c9be7
fagmai <mai.guimaraes99@gmail.com>
2023-06-29 22:39:18 -0300

    Teste

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de jogos");
 console.log("Aplicação rodando no banco de dados");
 console.log("Teste de modificação");
 console.log("Enviando para GitHub");
+console.log("Teste modificação 2");
