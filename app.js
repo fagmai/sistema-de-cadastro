acb5ced41b475d09063b85adfdaa8efb44242ea8
fagmai <mai.guimaraes99@gmail.com>
2023-06-29 22:28:33 -0300

    app.js

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos");
+console.log("Aplicação rodando no banco de dados");
+console.log("Teste de modificação");
+console.log("Enviando para GitHub");
